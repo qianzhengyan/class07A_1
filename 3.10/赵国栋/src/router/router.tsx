0e711b1aa19d7a7284b29c70c811ebb6dd8440b3
Guo Dong <953960983@qq.com>
2020-03-11 00:46:49 +0800

    shang

---
+++ 3.10/赵国栋/src/router/router.tsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class router extends Component {
+    render() {
+        return (
+            <div>
+                router
+            </div>
+        )
+    }
+}
